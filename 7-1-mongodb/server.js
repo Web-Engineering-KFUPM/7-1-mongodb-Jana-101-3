4e78d498ea394588221d61b17f07e6ce2269e329
jana alkahlan <jana.kahlan@gmail.com>
2026-04-13 13:52:48 +0300

    TODO2

--- 7-1-mongodb/server.js
+++ 7-1-mongodb/server.js
@@ -192,7 +192,13 @@ mongoose.connect("mongodb+srv://janakahlan_db_user:Test1234@cluster0.vunxiff.mon
   .catch(err => console.log(err));
   
 // establish connection
+const studentSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+  major: String
+});
 
+const Student = mongoose.model("Student", studentSchema);
 
 // define schema
 
